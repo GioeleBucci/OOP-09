ffbb86c85ff8f2b791a0311de6c910ae3dd38bc2
GioeleBucci <gioele.bucci@studio.unibo.it>
2023-11-24 19:22:43 +0100

    finish es 92

--- 92-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
+++ 92-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
@@ -10,12 +10,12 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.List;
 import java.util.Random;
 
 /**
@@ -27,78 +27,96 @@ import java.util.Random;
  */
 public class BadIOGUI {
 
-    private static final String TITLE = "A very simple GUI application";
-    private static final String PATH = System.getProperty("user.home")
-            + File.separator
-            + BadIOGUI.class.getSimpleName() + ".txt";
-    private static final int PROPORTION = 5;
-    private final Random randomGenerator = new Random();
-    private final JFrame frame = new JFrame(TITLE);
+  private static final String TITLE = "A very simple GUI application";
+  private static final String PATH = System.getProperty("user.home")
+      + File.separator
+      + BadIOGUI.class.getSimpleName() + ".txt";
+  private static final int PROPORTION = 5;
+  private final Random randomGenerator = new Random();
+  private final JFrame frame = new JFrame(TITLE);
 
-    /**
-     * Creates a new BadIOGUI.
+  /**
+   * Creates a new BadIOGUI.
+   */
+  public BadIOGUI() {
+    final JPanel canvas = new JPanel();
+    canvas.setLayout(new BorderLayout());
+    final JPanel secondaryCanvas = new JPanel();
+    secondaryCanvas.setLayout(new BoxLayout(secondaryCanvas, BoxLayout.X_AXIS));
+    final JButton write = new JButton("Write on file");
+    secondaryCanvas.add(write);
+    final JButton read = new JButton("Read from file");
+    secondaryCanvas.add(read);
+    canvas.add(secondaryCanvas, BorderLayout.CENTER);
+    frame.setContentPane(canvas);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    /*
+     * Handlers
      */
-    public BadIOGUI() {
-        final JPanel canvas = new JPanel();
-        canvas.setLayout(new BorderLayout());
-        final JButton write = new JButton("Write on file");
-        canvas.add(write, BorderLayout.CENTER);
-        frame.setContentPane(canvas);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    write.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(final ActionEvent e) {
         /*
-         * Handlers
+         * This would be VERY BAD in a real application.
+         * 
+         * This makes the Event Dispatch Thread (EDT) work on an I/O
+         * operation. I/O operations may take a long time, during which
+         * your UI becomes completely unresponsive.
          */
-        write.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                /*
-                 * This would be VERY BAD in a real application.
-                 * 
-                 * This makes the Event Dispatch Thread (EDT) work on an I/O
-                 * operation. I/O operations may take a long time, during which
-                 * your UI becomes completely unresponsive.
-                 */
-                try (PrintStream ps = new PrintStream(PATH, StandardCharsets.UTF_8)) {
-                    ps.print(randomGenerator.nextInt());
-                } catch (IOException e1) {
-                    JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
-                    e1.printStackTrace(); // NOPMD: allowed as this is just an exercise
-                }
-            }
-        });
-    }
+        try (PrintStream ps = new PrintStream(PATH, StandardCharsets.UTF_8)) {
+          ps.print(randomGenerator.nextInt());
+        } catch (IOException e1) {
+          JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
+          e1.printStackTrace(); // NOPMD: allowed as this is just an exercise
+        }
+      }
+    });
 
-    private void display() {
-        /*
-         * Make the frame one fifth the resolution of the screen. This very method is
-         * enough for a single screen setup. In case of multiple monitors, the
-         * primary is selected. In order to deal coherently with multimonitor
-         * setups, other facilities exist (see the Java documentation about this
-         * issue). It is MUCH better than manually specify the size of a window
-         * in pixel: it takes into account the current resolution.
-         */
-        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-        final int sw = (int) screen.getWidth();
-        final int sh = (int) screen.getHeight();
-        frame.setSize(sw / PROPORTION, sh / PROPORTION);
-        /*
-         * Instead of appearing at (0,0), upper left corner of the screen, this
-         * flag makes the OS window manager take care of the default positioning
-         * on screen. Results may vary, but it is generally the best choice.
-         */
-        frame.setLocationByPlatform(true);
-        /*
-         * OK, ready to push the frame onscreen
-         */
-        frame.setVisible(true);
-    }
+    read.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(final ActionEvent e) {
+        /* Usage of pattern decorator */
+        try (BufferedReader br = new BufferedReader(new FileReader(PATH, StandardCharsets.UTF_8))) {
+          System.out.println(br.readLine());
+        } catch (IOException exception) {
+        }
+      }
+    });
+  }
 
-    /**
-     * Launches the application.
-     *
-     * @param args ignored
+  private void display() {
+    /*
+     * Make the frame one fifth the resolution of the screen. This very method is
+     * enough for a single screen setup. In case of multiple monitors, the
+     * primary is selected. In order to deal coherently with multimonitor
+     * setups, other facilities exist (see the Java documentation about this
+     * issue). It is MUCH better than manually specify the size of a window
+     * in pixel: it takes into account the current resolution.
      */
-    public static void main(final String... args) {
-       new BadIOGUI().display();
-    }
+    final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+    final int sw = (int) screen.getWidth();
+    final int sh = (int) screen.getHeight();
+    frame.setSize(sw / PROPORTION, sh / PROPORTION);
+    /*
+     * Instead of appearing at (0,0), upper left corner of the screen, this
+     * flag makes the OS window manager take care of the default positioning
+     * on screen. Results may vary, but it is generally the best choice.
+     */
+    frame.setLocationByPlatform(true);
+    frame.pack(); // minimize window
+
+    /*
+     * OK, ready to push the frame onscreen
+     */
+    frame.setVisible(true);
+  }
+
+  /**
+   * Launches the application.
+   *
+   * @param args ignored
+   */
+  public static void main(final String... args) {
+    new BadIOGUI().display();
+  }
 }
